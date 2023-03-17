f9cef3c896113a9e04e8906f530e112ad808716b
s-channah <hannahsadofabohns@gmail.com>
2023-03-16 19:07:56 -0700

    using %zu specifier to print sizes of unsigned integers

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point for program execution
+ * Description: C program that prints various computer types it runs on
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
