3d089d7de293386f3c61bb165b93ce4879d68163
s-channah <hannahsadofabohns@gmail.com>
2023-03-16 18:07:50 -0700

    creating a C program using puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point for program execution
+ * Description - a C program that prints with the puts command
+ * Return - this is usually 0 as a default for successful program ran
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
